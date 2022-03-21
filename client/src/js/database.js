bafeb45c50ecda3ff48c825a712c4090bada64dd
Nicholas Krilis <n.krilis@gmail.com>
2022-03-20 23:22:30 -0400

    Completed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,39 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('nate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('nate')) {
+        console.log('nate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('nate', { keyPath: 'id', autoIncrement: true });
+      console.log('nate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => 
+{
+  console.log('PUT to the database');
+  const nateDB = await openDB('nate', 1);
+  const tx = nateDB.transaction('nate', 'readwrite');
+  const store = tx.objectStore('nate');
+  const request = store.put({ nate: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => 
+{
+  console.log('GET all from the database');
+  const nateDB = await openDB('nate', 1);
+  const tx = nateDB.transaction('nate', 'readonly');
+  const store = tx.objectStore('nate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,12 +19,60 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+      new MiniCssExtractPlugin(),
+      // TODO: Add InjectManifest Plugin
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      new WebpackPwaManifest(
+        {
+          name: 'Just Another Text Editor',
+          short_name: 'JATE',
+          description: 'My awesome text editor!',
+          background_color: '#ffffff',
+          start_url: '/',
+          publicPath: '/',
+          fingerprints: false,
+          inject: true,
+          icons: [
+            {
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'), // multiple sizes
+            },
+          ]
+        }),
       
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
